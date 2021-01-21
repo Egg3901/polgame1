c305dcddbb17fdd2a5fd753a596ebe1989fb9835
Egg <jlpower112@gmail.com>
2021-01-21 14:07:04 -0500

    Update state.php

--- state.php
+++ state.php
@@ -5,12 +5,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 $state = $_GET['state'];
-$imgsource = ""img/state/$state"";
+$imgsource = ""img/states/$state"";
 
 echo "<div class ='state-header'>
 <h1>The State of  " . $state . " </h1>
 
-<img src=$imgsource>
+<img src=$imgsource alt='state image'>
 
 </tr>";
 
