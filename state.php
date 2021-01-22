73eaa2fdb810b5288a3e0e4961588e2518d96e4d
Egg <jlpower112@gmail.com>
2021-01-22 13:37:02 -0500

    Update state.php

--- state.php
+++ state.php
@@ -32,15 +32,83 @@ echo "
 ";
 
 
-$uquery = 'SELECT influence, polname, social, economic FROM accounts WHERE polstate = ?';
+$uquery = 'SELECT influence, polname, social, economic FROM accounts WHERE polstate = ? ORDER BY influence';
 $stmt = $con->prepare($uquery);
 $stmt->bind_param("s", $state);
 $stmt->execute();
 $result = $stmt->get_result();
+
 foreach ($result as $row) {
     print " <tr> ";
+    $iter = 0;
     foreach ($row as $polname=>$data){
-        print " <td> $data</td> ";
+        if ($iter = 0) {  //influence
+            print " <td> $data % </Td>";
+        }
+        if ($iter = 1) {  //name
+            print " <td> $data </td> ";
+        }
+        if ($iter = 2) { // social position data on state page
+            $social = $data;
+            if ($social <= 5){
+                $formattedsocial = "Very Right Wing";
+            }
+            if ($social <= 4){
+                $formattedsocial = "Right Wing";
+            }
+            if ($social <= 3){
+                $formattedsocial = "Leans Right Wing";
+            }
+            if ($social <= 1){
+                $formattedsocial = "Center Right";
+            }
+            if ($social <= 0){
+                $formattedsocial = "Centrist";
+            }
+            if ($social <= -1){
+                $formattedsocial = "Center Left";
+            }
+            if ($social <= -3){
+                $formattedsocial = "Leans Left Wing";
+            }
+            if ($social <= -4){
+                $formattedsocial = "Left Wing";
+            }
+            if ($social <= -5) {
+                $formattedsocial = "Libertarian Left";
+            }
+            print " <td> $formattedsocial </td> ";
+        }
+        if ($iter = 3){
+            $economic = $data;
+            if ($economic <= 5){
+                $formattedeconomic = "Libertarian Right";
+            }
+            if ($economic <= 4){
+                $formattedeconomic = "Right Wing";
+            }
+            if ($economic <= 3){
+                $formattedeconomic = "Leans Right Wing";
+            }
+            if ($economic <= 1){
+                $formattedeconomic = "Center Right";
+            }
+            if ($economic <= 0){
+                $formattedeconomic = "Centrist";
+            }
+            if ($economic <= -1){
+                $formattedeconomic = "Center Left";
+            }
+            if ($economic <= -3){
+                $formattedeconomic = "Leans Left Wing";
+            }
+            if ($economic <= -4){
+                $formattedeconomic = "Left Wing";
+            }
+            if ($economic <= -5){
+                $formattedeconomic = "Very Left Wing";
+            }
+        }
     }
     print " </tr> ";
 }
