b283e84e262c5f7f88b9c5a19b19ba8842eee5b6
Egg <jlpower112@gmail.com>
2021-01-23 11:12:49 -0500

    Update increaserecognition.php

--- adminscripts/increaserecognition.php
+++ adminscripts/increaserecognition.php
@@ -5,8 +5,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
 $con = OpenCon();
-$uquery = 'UPDATE accounts SET influence = ((influencebase * .1) + 1) + influencebase AND SET influencebase = influencebase + 0.5';
+$uquery = 'UPDATE accounts SET influence = 1 + influencebase * .1 + influencebase AND SET influencebase = influencebase + 0.5  WHERE polname = ?';
 $stmt = $con->prepare($uquery);
+$stmt->bind_param('i', $_SESSION['polname']);
 $stmt->execute();
 $stmt->fetch();
 $stmt->close();
