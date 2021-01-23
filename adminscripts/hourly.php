38cbb00251a403c979df5c3856e69d4f254c2386
Egg <jlpower112@gmail.com>
2021-01-23 13:02:15 -0500

    Update hourly.php

--- adminscripts/hourly.php
+++ adminscripts/hourly.php
@@ -3,20 +3,32 @@ include 'connect.php';
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
+
 $con = OpenCon();
 $uquery = 'UPDATE accounts SET influence = influence * .97';
 $stmt = $con->prepare($uquery);
 $stmt->execute();
 $stmt->fetch();
 $stmt->close();
+
 $con = OpenCon();
 $uquery = 'UPDATE accounts SET action = action + 5';
 $stmt = $con->prepare($uquery);
 $stmt->execute();
 $stmt->fetch();
 $stmt->close();
+
 $con = OpenCon();
 $uquery = 'UPDATE accounts SET recognitionbase = recognitionbase * .8';
+$stmt = $con->prepare($uquery);
+$stmt->execute();
+$stmt->fetch();
+$stmt->close();
+
+$con = OpenCon();
+$uquery = 'UPDATE accounts SET action = action + 5 WHERE seat CONTAINS ? ';
+$seat = "Governor";
+$stmt->bind_param('s', $seat); // gets the id var from the current session, binds it to the
 $stmt = $con->prepare($uquery);
 $stmt->execute();
 $stmt->fetch();
