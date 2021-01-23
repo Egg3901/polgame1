d551dddeceb4b9ee95648a10151d3b21f6283b0e
Egg <jlpower112@gmail.com>
2021-01-23 12:37:15 -0500

    Update campaignoffice.php

--- campaignoffice.php
+++ campaignoffice.php
@@ -10,7 +10,8 @@ include 'navigation.php';
             <h1 style="margin: auto;"> Build Campaign Infrastructure</h1><br>
             <img src="img/mikeoffice" alt="camp image" style="margin:auto;" border=".5"><br>
             <p>Increasing your campaign infrastructure will increase your name recognition in your state. <br>
-                    Campaign offices will distribute signs, and register volunteers to canvas.</p>
+                    Campaign offices will distribute signs, and register volunteers to canvas.</p><br>
+            <p><strong> Costs 5 actions, 0 funding. </strong></p>
     </div>
 
 </form>
