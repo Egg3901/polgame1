24be24d13ad314e3a5931b727df95aef8025d9d9
Egg <jlpower112@gmail.com>
2021-01-23 12:23:55 -0500

    update

--- campaignoffice.php
+++ campaignoffice.php
@@ -4,6 +4,7 @@ include 'navigation.php';
 ?>
 <!DOCTYPE HTML>
 <form action="adminscripts/increaserecognition.php">
-    <input type="submit" value="Increase Campaign Infrastructure">
-    <p> Increasing your campaign infrastructure will increase your name recognition in your state. Campaign offices will distribute signs, and register volunteers to canvas.</p>
+
+
+    <p> <input type="submit" value="Increase Campaign Infrastructure"> <img src="img/mikeoffice">Increasing your campaign infrastructure will increase your name recognition in your state. Campaign offices will distribute signs, and register volunteers to canvas.</p>
 </form>
