b3658ae7d2a55b8f1b9a6b3cefd5e556ca74500f
Egg <jlpower112@gmail.com>
2021-01-23 11:38:32 -0500

    Update increaserecognition.php

--- adminscripts/increaserecognition.php
+++ adminscripts/increaserecognition.php
@@ -5,14 +5,24 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
 $con = OpenCon();
-$uquery = 'UPDATE accounts SET recognitionbase = recognitionbase + 0.5 WHERE id = ?';
-$stmt = $con->prepare($uquery);
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->fetch();
-$uquery = 'UPDATE accounts SET influence = influence + 1 + (recognitionbase * .1) WHERE id = ?';
-$stmt = $con->prepare($uquery);
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->fetch();
-$stmt->close();
+$det = $con->prepare('SELECT action FROM accounts WHERE id = ?');
+$det->bind_param('i', $_SESSION['id']); // gets the id var from the current session, binds it to the
+$det->execute();
+$det->bind_result($action);
+if ($action < 5) {
+    header('Location: campaignoffice.php');
+}
+if ($action > 5){
+    $uquery = 'UPDATE accounts SET recognitionbase = recognitionbase + 0.5 WHERE id = ?';
+    $stmt = $con->prepare($uquery);
+    $stmt->bind_param('i', $_SESSION['id']);
+    $stmt->execute();
+    $stmt->fetch();
+    $uquery = 'UPDATE accounts SET influence = influence + 1 + (recognitionbase * .1) WHERE id = ?';
+    $stmt = $con->prepare($uquery);
+    $stmt->bind_param('i', $_SESSION['id']);
+    $stmt->execute();
+    $stmt->fetch();
+    $stmt->close();
+}
+$det->close();
