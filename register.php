c14e4b57eb5841d8fa6b977a0872b6e00ce87bd4
Egg <jlpower112@gmail.com>
2021-01-23 12:10:45 -0500

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-include 'connect.php';
+include 'adminscripts/connect.php';
 $con = OpenCon();
 if ( !isset($_POST['username'], $_POST['password']) ) {
     // Could not get the data that should have been sent.
