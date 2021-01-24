7c03628b2050e5af30c8c7262bf0e01f513c561e
Egg <jlpower112@gmail.com>
2021-01-23 22:07:20 -0500

    css

--- campaignoffice.php
+++ campaignoffice.php
@@ -3,7 +3,7 @@
 include 'navigation.php';
 ?>
 <!DOCTYPE HTML>
-<div>
+<div class="main">
     <form action="adminscripts/increaserecognition.php">
         <div style="margin: auto; width=30%;">
 
@@ -16,15 +16,14 @@ include 'navigation.php';
         </div>
     </form>
     <form action="adminscripts/increasedonorbase.php">
-    <div style="margin: auto; width=30%;">
-
-        <button type="submit" ><br>
-            <h1 style="margin: auto;"> Appeal To Grassroots Donors</h1><br>
-            <img src="img/mikeoffice" alt="camp image" style="margin:auto;" border=".5"><br>
-            <p>Appealing to grassroots donors increases your hourly income from them in the form of donations.<br>
-                Grasroots appeal is lowered by appealing to corporate donors, and is less effective for moderate candidates.</p><br>
-            <p><strong> Costs 10 actions. </strong></p>
-    </div>
+        <div style="margin: auto; width=30%;">
+            <button type="submit" ><br>
+                <h1 style="margin: auto;"> Appeal To Grassroots Donors</h1><br>
+                <img src="img/mikeoffice" alt="camp image" style="margin:auto;" border=".5"><br>
+                <p>Appealing to grassroots donors increases your hourly income from them in the form of donations.<br>
+                    Grasroots appeal is lowered by appealing to corporate donors, and is less effective for moderate candidates.</p><br>
+                <p><strong> Costs 10 actions. </strong></p>
+        </div>
     </form>
 </div>
 
