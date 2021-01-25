1aa8126f7f270a07dcabe78d054fe7aba4c597ba
EggPC <unconfigured@null.spigotmc.org>
2021-01-25 16:45:44 -0500

    Update state.php

--- state.php
+++ state.php
@@ -192,7 +192,7 @@ echo "
             
         </table>
         <div style='text-align: center;'>
-                <form  action='adminscripts/joingovrace.php' class='race' style='margin: auto; width: 11%'>
+                <form  action='adminscripts/joingovrace.php' class='race' style='margin: auto; width: 10%'>
                         <button type='submit' value='Join Race' style='margin: auto; width: 100%'> Join Gubernatorial Race</button>
                 </form>
         </div>
