38d1e6422e22a32c51a7ff83786c63581110d894
EggPC <unconfigured@null.spigotmc.org>
2021-01-25 18:58:44 -0500

    Update campaignoffice.php

--- campaignoffice.php
+++ campaignoffice.php
@@ -4,39 +4,66 @@ include 'navigation.php';
 ?>
 <!DOCTYPE HTML>
 <table>
-    <th>Your Political Party</th>
-    <th>Party Alignment</th>
-    <?php
-        $con = OpenCon(); // opens a connection to the database, this function is from the above included script
-        $stmt = $con->prepare('SELECT polstate, polname FROM accounts WHERE id = ?');
-        $stmt->bind_param('i', $_SESSION['id']);
-        $stmt->execute();
-        $stmt->bind_result($polstate, $polname);
+    <style>
+        .grid-container {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            grid-template-rows: 1fr 1fr 1fr;
+            gap: 0px 0px;
+            grid-template-areas:
+    "-heavy -heavy -heavy"
+    "campinf funding ."
+    ". . .";
+        }
+        .-heavy { grid-area: -heavy; }
+        .campinf { grid-area: campinf; }
+        .funding { grid-area: funding; }
 
-    ?>
-</table>
-<div class="main">
-    <form action="adminscripts/increaserecognition.php">
-        <div style=" width=20%; height=auto;">
+    </style>
 
-            <button type="submit" ><br>
-                <h1 style="margin: auto;"> Build Campaign Infrastructure</h1><br>
-                <img src="img/mikeoffice" alt="camp image" style="margin:auto;" border=".5"><br>
-                <p>Increasing your campaign infrastructure will increase your name recognition in your state. <br>
-                        Campaign offices will distribute signs, and register volunteers to canvas.</p><br>
-                <p><strong> Costs 5 actions, 0 funding. </strong></p>
+    <div class="grid-container">
+        <div class="-heavy">
+            <table>
+                <th>Your Political Party</th>
+                <th>Party Alignment</th>
+                <?php
+                $con = OpenCon(); // opens a connection to the database, this function is from the above included script
+                $stmt = $con->prepare('SELECT polstate, polname FROM accounts WHERE id = ?');
+                $stmt->bind_param('i', $_SESSION['id']);
+                $stmt->execute();
+                $stmt->bind_result($polstate, $polname);
+
+                ?>
+            </table>
         </div>
-    </form>
-    <form action="adminscripts/increasedonor.php">
-        <div style=" width=20%; height=auto;">
-            <button type="submit" ><br>
-                <h1 style="margin: auto;"> Appeal To Grassroots Donors</h1><br>
-                <img src="img/mikeoffice" alt="camp image" style="margin:auto;" border=".5"><br>
-                <p>Appealing to grassroots donors increases your hourly income from them in the form of donations.<br>
-                    Grasroots appeal is lowered by appealing to corporate donors, and is less effective for moderate candidates.</p><br>
-                <p><strong> Costs 10 actions. </strong></p>
+
+        <div class="campinf">
+            <form action="adminscripts/increaserecognition.php">
+                <div style=" width=20%; height=auto;">
+
+                    <button type="submit" ><br>
+                        <h1 style="margin: auto;"> Build Campaign Infrastructure</h1><br>
+                        <img src="img/campaignoffice/mikeoffice" alt="camp image" style="margin:auto;" border=".5"><br>
+                        <p>Increasing your campaign infrastructure will increase your name recognition in your state. <br>
+                            Campaign offices will distribute signs, and register volunteers to canvas.</p><br>
+                        <p><strong> Costs 5 actions, 0 funding. </strong></p>
+                </div>
+            </form>
         </div>
-    </form>
-</div>
+        
+        <div class="funding">
+            <form action="adminscripts/increasedonor.php">
+                <div style=" width=20%; height=auto;">
+                    <button type="submit" ><br>
+                        <h1 style="margin: auto;"> Appeal To Grassroots Donors</h1><br>
+                        <img src="img/campaignoffice/donorbase" alt="donorbase image" style="margin:auto;" border=".5"><br>
+                        <p>Appealing to grassroots donors increases your hourly income from them in the form of donations.<br>
+                            Grasroots appeal is lowered by appealing to corporate donors, and is less effective for moderate candidates.</p><br>
+                        <p><strong> Costs 10 actions. </strong></p>
+                </div>
+            </form>
+        </div>
+    </div>
+
 
 
