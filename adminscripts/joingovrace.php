27a418fb06c09ccd5d88fca4dff14f3b38ccf5e0
EggPC <unconfigured@null.spigotmc.org>
2021-01-25 12:46:52 -0500

    Update joingovrace.php

--- adminscripts/joingovrace.php
+++ adminscripts/joingovrace.php
@@ -3,19 +3,10 @@ include '../adminscripts/connect.php';
 
 
 $con = OpenCon();
-$det = $con->prepare('SELECT polstate FROM accounts WHERE id = ?');
+$det = $con->prepare('SET rrace = 1 FROM accounts WHERE id = ?');
 $det->bind_param('i', $_SESSION['id']); // gets the id var from the current session, binds it to the
 $det->execute();
-$det->bind_result($statename);
 $det->fetch();
 $det->close();
 
-$con = OpenCon();
-$det = $con->prepare('INSERT INTO statexpartyrel VALUES (?,?,?,?,?)');
-$inc = 0;
-$party = 1;
-$race = 1;
-$det->bind_param('isiii', $_SESSION['id'],$statename,$inc,$party,$race); // gets the id var from the current session, binds it to the
-$det->execute();
-$det->fetch();
-$det->close();
+
