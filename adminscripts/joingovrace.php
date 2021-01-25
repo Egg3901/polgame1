3f0177dc1bafebddc83876a22c1a8f8abec8dfc9
EggPC <unconfigured@null.spigotmc.org>
2021-01-25 12:49:20 -0500

    Update joingovrace.php

--- adminscripts/joingovrace.php
+++ adminscripts/joingovrace.php
@@ -3,7 +3,7 @@ include '../adminscripts/connect.php';
 
 
 $con = OpenCon();
-$det = $con->prepare('SET rrace = 1 FROM accounts WHERE id = ?');
+$det = $con->prepare('UPDATE accounts SET rrace = 1 WHERE id = ?');
 $det->bind_param('i', $_SESSION['id']); // gets the id var from the current session, binds it to the
 $det->execute();
 $det->fetch();
