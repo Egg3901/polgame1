7e8e55314826ec297ff22351b831b10d81a24246
EggPC <unconfigured@null.spigotmc.org>
2021-01-25 12:51:25 -0500

    Update joingovrace.php

--- adminscripts/joingovrace.php
+++ adminscripts/joingovrace.php
@@ -8,5 +8,13 @@ $det->bind_param('i', $_SESSION['id']); // gets the id var from the current sess
 $det->execute();
 $det->fetch();
 $det->close();
+$con = OpenCon();
+$det = $con->prepare('SELECT polstate FROM accounts WHERE id = ?');
+$det->bind_param('i', $_SESSION['id']); // gets the id var from the current session, binds it to the
+$det->execute();
+$det->bind_result($state)
+$det->fetch();
+$det->close();
+header("Location: state.php?state=$state");
 
 
