9abc3dd0ccd4b36b68aff82033a939d286a23574
EggPC <unconfigured@null.spigotmc.org>
2021-01-25 19:03:06 -0500

    Update campaignoffice.php

--- campaignoffice.php
+++ campaignoffice.php
@@ -10,7 +10,9 @@ include 'navigation.php';
             grid-template-columns: 1fr 1fr 1fr;
             grid-template-rows: 1fr 1fr 1fr;
             gap: 0px 0px;
+            word-wrap: break-word;
             grid-template-areas:
+
     "-heavy -heavy -heavy"
     "campinf funding ."
     ". . .";
