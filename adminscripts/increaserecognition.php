f6e8b27cd605ed0e4be230785715a87d13733521
Egg3901 <jlpower112@gmail.com>
2021-01-27 17:55:26 -0500

    Update increaserecognition.php

--- adminscripts/increaserecognition.php
+++ adminscripts/increaserecognition.php
@@ -5,35 +5,41 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
 $con = OpenCon();
-$det = $con->prepare('SELECT action FROM accounts WHERE id = ?');
+$det = $con->prepare('SELECT action, influence FROM accounts WHERE id = ?');
 $det->bind_param('i', $_SESSION['id']); // gets the id var from the current session, binds it to the
 $det->execute();
-$det->bind_result($action);
+$det->bind_result($action, $influence);
 $det->fetch();
 $det->close();
-if ($action < 5) {
+if ( $influence < 98){
+    if ($action < 5) {
+        header('Location: ../profile.php');
+    }
+    if ($action >= 5){
+        $uquery = 'UPDATE accounts SET recognitionbase = recognitionbase + 0.5 WHERE id = ?';
+        $stmt = $con->prepare($uquery);
+        $stmt->bind_param('i', $_SESSION['id']);
+        $stmt->execute();
+        $stmt->fetch();
+        $stmt->close();
+        $uquery = 'UPDATE accounts SET influence = influence + 1 + (recognitionbase * .1) WHERE id = ?';
+        $stmt = $con->prepare($uquery);
+        $stmt->bind_param('i', $_SESSION['id']);
+        $stmt->execute();
+        $stmt->fetch();
+        $stmt->close();
+        $action = round($action - 5);
+        $uquery = 'UPDATE accounts SET action = ? WHERE id = ? ';
+        $stmt = $con->prepare($uquery);
+        $stmt->bind_param('ii', $action,$_SESSION['id']);
+        $stmt->execute();
+        $stmt->fetch();
+        $stmt->close();
+        header('Location: ../campaignoffice.php');
+
+    }
+}
+else {
     header('Location: ../profile.php');
 }
-if ($action >= 5){
-    $uquery = 'UPDATE accounts SET recognitionbase = recognitionbase + 0.5 WHERE id = ?';
-    $stmt = $con->prepare($uquery);
-    $stmt->bind_param('i', $_SESSION['id']);
-    $stmt->execute();
-    $stmt->fetch();
-    $stmt->close();
-    $uquery = 'UPDATE accounts SET influence = influence + 1 + (recognitionbase * .1) WHERE id = ?';
-    $stmt = $con->prepare($uquery);
-    $stmt->bind_param('i', $_SESSION['id']);
-    $stmt->execute();
-    $stmt->fetch();
-    $stmt->close();
-    $action = round($action - 5);
-    $uquery = 'UPDATE accounts SET action = ? WHERE id = ? ';
-    $stmt = $con->prepare($uquery);
-    $stmt->bind_param('ii', $action,$_SESSION['id']);
-    $stmt->execute();
-    $stmt->fetch();
-    $stmt->close();
-    header('Location: ../campaignoffice.php');
 
-}
