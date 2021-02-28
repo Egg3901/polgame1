b72bd1fcdccd6b0ac093504b87a84551fb1fb78d
Egg3901 <jlpower112@gmail.com>
2021-02-28 14:47:33 -0500

    Update formatting_functions.php

--- common_functions/formatting_functions.php
+++ common_functions/formatting_functions.php
@@ -9,34 +9,17 @@
  */
 
 function formatPosition($position): array
-{
-    if ($position <= 5) : {
-        $formatted_position = "Very Right Wing";
+{{
+    if ($position <= -5) : {
+        $formatted_position = "Libertarian Left";
         $color = "#DD491B";
         return array($formatted_position, $color);
-    } elseif ($position <= 4) : {
-        $formatted_position = "Right Wing";
+    } elseif ($position <= -5) : {
+        $formatted_position = "Very Left Wing";
         $color = "#DD491B";
         return array($formatted_position, $color);
-    } elseif ($position <= 3) : {
-        $formatted_position = "Leans Right Wing";
-        $color = "#DD491B";
-        return array($formatted_position, $color);
-    }
-
-    elseif ($position <= 1) : {
-        $formatted_position = "Center Right";
-        $color = "#DD491B";
-        return array($formatted_position, $color);
-    }
-
-    elseif ($position <= 0) : {
-        $formatted_position = "Centrist";
-        $color = "#DD491B";
-        return array($formatted_position, $color);
-    }
-    elseif ($position <= -1) : {
-        $formatted_position = "Center Left";
+    } elseif ($position <= -4) : {
+        $formatted_position = "Left Wing";
         $color = "#DD491B";
         return array($formatted_position, $color);
     }
@@ -45,19 +28,50 @@ function formatPosition($position): array
         $color = "#DD491B";
         return array($formatted_position, $color);
     }
-    elseif ($position <= -4) : {
-        $formatted_position = "Left Wing";
+    elseif ($position <= 3) : {
+        $formatted_position = "Leans Right Wing";
         $color = "#DD491B";
         return array($formatted_position, $color);
     }
-    elseif ($position <= -5) : {
-        $formatted_position = "Libertarian Left";
+    elseif ($position <= -1) : {
+        $formatted_position = "Center Left";
+        $color = "#DD491B";
+        return array($formatted_position, $color);
+    }
+
+
+    elseif ($position <= 0) : {
+        $formatted_position = "Centrist";
+        $color = "#DD491B";
+        return array($formatted_position, $color);
+    }
+    elseif ($position <= 1) : {
+        $formatted_position = "Center Right";
+        $color = "#DD491B";
+        return array($formatted_position, $color);
+    }
+    elseif ($position <= 3) : {
+        $formatted_position = "Leans Right Wing";
+        $color = "#DD491B";
+        return array($formatted_position, $color);
+    }
+    elseif ($position <= 4) : {
+        $formatted_position = "Right Wing";
+        $color = "#DD491B";
+        return array($formatted_position, $color);
+    }
+    else: {
+        $formatted_position = "Very Right Wing";
         $color = "#DD491B";
         return array($formatted_position, $color);
     }
     endif;
+}}
 
-}
+
+
+
+
 
 
 
