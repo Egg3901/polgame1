0a03e80baa37ebed367ed7c3812bbf4c4c9d1dcf
Egg3901 <jlpower112@gmail.com>
2021-02-28 14:25:24 -0500

    fixed profile page

--- common_functions/scrape_profile_data.php
+++ common_functions/scrape_profile_data.php
@@ -7,7 +7,7 @@ function fetchProfile($profile_id): array {
     // this function takes a profile ID, and fetches their information from the db
     if (is_null($profile_id)) {
         $con = OpenCon(); // opens a connection to the database, this function is from the above included script
-        $stmt = $con->prepare('SELECT email, influence, polstate, polname, imgurl, social, economic, action, funding, party FROM accounts WHERE id = ?');
+        $stmt = $con->prepare('SELECT influence, polstate, polname, imgurl, social, economic, action, funding, party FROM accounts WHERE id = ?');
         $stmt->bind_param('i', $id); // gets the id var from the current session, binds it to the
         $stmt->execute();
         $stmt->bind_result( $influence, $home_state, $polname, $imgurl, $social, $economic, $ap, $funds, $partyid);
