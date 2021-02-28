c7b4b35c2b5d111fe286144e74627ed49152e488
Egg3901 <jlpower112@gmail.com>
2021-02-28 14:53:33 -0500

    Update formatting_functions.php

--- common_functions/formatting_functions.php
+++ common_functions/formatting_functions.php
@@ -11,58 +11,48 @@
 function formatPosition($position): array
 {{
     if ($position <= -5) : {
-        $formatted_position = "Libertarian Left";
-        $color = "#DD491B";
-        return array($formatted_position, $color);
-    } elseif ($position <= -5) : {
         $formatted_position = "Very Left Wing";
-        $color = "#DD491B";
+        $color = "#005CA2";
         return array($formatted_position, $color);
     } elseif ($position <= -4) : {
         $formatted_position = "Left Wing";
-        $color = "#DD491B";
+        $color = "#009BF8";
         return array($formatted_position, $color);
     }
     elseif ($position <= -3) : {
         $formatted_position = "Leans Left Wing";
-        $color = "#DD491B";
-        return array($formatted_position, $color);
-    }
-    elseif ($position <= 3) : {
-        $formatted_position = "Leans Right Wing";
-        $color = "#DD491B";
+        $color = "#6DACFE";
         return array($formatted_position, $color);
     }
     elseif ($position <= -1) : {
         $formatted_position = "Center Left";
-        $color = "#DD491B";
+        $color = "#A4CCFF";
         return array($formatted_position, $color);
     }
 
-
     elseif ($position <= 0) : {
         $formatted_position = "Centrist";
-        $color = "#DD491B";
+        $color = "#A5A5A5";
         return array($formatted_position, $color);
     }
     elseif ($position <= 1) : {
         $formatted_position = "Center Right";
-        $color = "#DD491B";
+        $color = "#FF9695 ";
         return array($formatted_position, $color);
     }
     elseif ($position <= 3) : {
         $formatted_position = "Leans Right Wing";
-        $color = "#DD491B";
+        $color = "#F56D6B";
         return array($formatted_position, $color);
     }
     elseif ($position <= 4) : {
         $formatted_position = "Right Wing";
-        $color = "#DD491B";
+        $color = "#FE3B38";
         return array($formatted_position, $color);
     }
     else: {
         $formatted_position = "Very Right Wing";
-        $color = "#DD491B";
+        $color = "#FD211D";
         return array($formatted_position, $color);
     }
     endif;
