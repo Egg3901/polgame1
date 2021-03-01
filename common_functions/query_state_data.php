496d9b48a94f32690e17a748266bf2697cd1cb66
Egg <jlpower112@gmail.com>
2021-03-01 10:13:00 -0500

    Revert "test"
    This reverts commit e5b8e50eaa057d8a9870cf5f18ef4319b961b730.

--- common_functions/query_state_data.php
+++ common_functions/query_state_data.php
@@ -1,7 +1,1 @@
 <?php
-if (!isset($_SESSION['loggedin'])) {
-    header('Location: index.html');
-    exit;
-}
-function fetchStateInfo($): array {
-    // this function takes a profile ID, and fetches their information from the db
