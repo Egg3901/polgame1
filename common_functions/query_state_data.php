e5b8e50eaa057d8a9870cf5f18ef4319b961b730
Egg <jlpower112@gmail.com>
2021-02-28 22:50:30 -0500

    test

--- common_functions/query_state_data.php
+++ common_functions/query_state_data.php
@@ -1,1 +1,7 @@
 <?php
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.html');
+    exit;
+}
+function fetchStateInfo($): array {
+    // this function takes a profile ID, and fetches their information from the db
